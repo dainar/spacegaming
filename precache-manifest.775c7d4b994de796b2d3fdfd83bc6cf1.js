c397d5c6ff7b727188960654d3221eb031341636
adriangp <agonzalezpuig@amarello.com.mx>
2019-02-14 16:01:48 -0600

    Updates

---
+++ precache-manifest.775c7d4b994de796b2d3fdfd83bc6cf1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e10ac656eff4546b8b2529be50abe5e7",
+    "url": "/spacegaming/static/media/tick.e10ac656.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spacegaming/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "916865086a99d3df891fda4bd5ac5e9e",
+    "url": "/spacegaming/static/media/lock.91686508.svg"
+  },
+  {
+    "revision": "93a1514185e4abe4574259f8f6c11907",
+    "url": "/spacegaming/static/media/clock.93a15141.svg"
+  },
+  {
+    "revision": "ff8363e18ef45f29dcd4",
+    "url": "/spacegaming/static/js/runtime~main.ff8363e1.js"
+  },
+  {
+    "revision": "ad4c03d6e62c24f468c8",
+    "url": "/spacegaming/static/js/main.ad4c03d6.chunk.js"
+  },
+  {
+    "revision": "84333e644dac37c93fcb",
+    "url": "/spacegaming/static/js/2.84333e64.chunk.js"
+  },
+  {
+    "revision": "ad4c03d6e62c24f468c8",
+    "url": "/spacegaming/static/css/main.add528c9.chunk.css"
+  },
+  {
+    "revision": "aab837aa039fee05c3e3e83cd0b3fcf3",
+    "url": "/spacegaming/index.html"
+  }
+];
